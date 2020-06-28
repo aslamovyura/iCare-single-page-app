475c2485560b239261d295ab55aaa960ce823818
Aslamov Yury <aslamov.yura@gmail.com>
2020-06-28 16:23:11 +0300

    feat: add page layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
      
 @Component({
     selector: 'icare-app',
-    templateUrl: './app.component.html'
+    templateUrl: './app.component.html',
+    styleUrls: ['../styles.css']
 })
 export class AppComponent { 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,28 +9,35 @@ import { HttpClientModule }   from '@angular/common/http';
 import { AppComponent }   from './app.component';
 
 import { NavComponent }   from './components/navigation/nav.component';
+import { FooterComponent}   from './components/footer/footer.component'
 
 import { HomeComponent }   from './components/home/home.component';
-import { LoginComponent }   from './components/login/login.component';
-import { PatientsComponent }   from './components/patients/patients.component';
-import { RecordsComponent }   from './components/records/records.component';
-import { RegisterComponent }   from './components/register/register.component';
-import { ReportsComponent }   from './components/reports/reports.component';
+import { NotFoundComponent} from './components/not-found/not-found.component';
+
 import { SensorsComponent }   from './components/sensors/sensorts.component';
+import { RecordsComponent }   from './components/records/records.component';
+import { PatientsComponent }   from './components/patients/patients.component';
+import { ReportsComponent }   from './components/reports/reports.component';
+
+import { LoginComponent }   from './components/login/login.component';
+import { RegisterComponent }   from './components/register/register.component';
+
 
 const appRoutes: Routes =[
     { path: 'home', component: HomeComponent},
-    { path: 'login', component: LoginComponent},
-    { path: 'patients', component: PatientsComponent},
-    { path: 'records', component: RecordsComponent},
-    { path: 'reports', component: ReportsComponent},
-    { path: 'register', component: RegisterComponent},
     { path: 'sensors', component: SensorsComponent},
+    { path: 'records', component: RecordsComponent},
+    { path: 'patients', component: PatientsComponent}, 
+    { path: 'reports', component: ReportsComponent},
+    { path: 'login', component: LoginComponent},
+    { path: 'register', component: RegisterComponent},
+    { path: '**', component: NotFoundComponent},
 ];
 
 @NgModule({
     imports:      [ BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
-    declarations: [ AppComponent, NavComponent, HomeComponent, LoginComponent, PatientsComponent, RecordsComponent, RegisterComponent, ReportsComponent, SensorsComponent],
+    declarations: [ AppComponent, NavComponent, FooterComponent, HomeComponent, NotFoundComponent,SensorsComponent, 
+                    RecordsComponent, PatientsComponent, ReportsComponent, RegisterComponent, LoginComponent],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
